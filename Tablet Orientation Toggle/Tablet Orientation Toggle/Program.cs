7e92532653088b6065b8f299f55dc66df796f479
MushMaw <jtbakker@ualberta.ca>
2020-06-12 20:28:17 -0600

    The init project

---
+++ Tablet Orientation Toggle/Tablet Orientation Toggle/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Threading.Tasks;
+
+namespace Tablet_Orientation_Toggle
+{
+	class Constants
+	{
+		public const string PrefPath = @"{0}\Users\{1}\AppData\Roaming\WTablet\wacom_tablet.dat";
+		public const string ServiceName = "WTabletServicePro.exe";
+		public const string HeyGit = "CommitThisPlz";
+	}
+	class TabletSettingsManager
+	{
+
+	}
+
+	class HotkeyToggle
+	{
+		static void Main(string[] args)
+		{
+
+		}
+	}
+}
